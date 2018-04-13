a3a07c264ff2c7a72289af7ef63ec5db881f6be9
otmane <otmane.ez@outlook.fr>
2018-04-13 11:24:00 +0200

    push site

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Table de multiplication</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        
+        <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+
+         <link href="https://fonts.googleapis.com/css?family=Gugi" rel="stylesheet">  
+         <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+         <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> 
+         <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet"> 
+
+    </head>
+    <body class="container-fluid">
+        
+        <section class="container text-center">
+
+            <header class="text-center">
+                <ul id="navigation">
+                    <li><a disabled="disabled">Accueil</a></li>
+                    <li><a  href="quiz.php">Quiz</a><li>
+                </ul>
+                <h3>Apprendre les tables de multiplication</h3>
+
+            </header>
+            
+
+            <div class=" text-center">
+                <!-- menu deroulante   -->
+                <form action="index.php" method="post">
+                    
+                    <?php
+                        echo "<SELECT name='valu' value='.$i.'>";
+                        
+                        for($i=1; $i<11; $i++){   
+                            echo "<OPTION>$i<br></OPTION>"; 
+        
+                        }
+                        echo "</SELECT>";  
+                                  
+                    ?>
+                    
+                    <input type="submit" value="Affichez" />
+
+                </form><br>
+                <?php
+                    // menu deroulante    
+                    if (isset($_POST["valu"])) {
+                        $a=$_POST["valu"];
+                    }else {
+                        $a=3;
+                    } 
+
+                        echo "<h4>Table de multiplication de ".$a." <h4>";
+                        echo "<table>";
+
+                        for($i=1;$i<=10;$i++) {
+                            $r=$a*$i;
+                            echo "<tr><td> ".$i." </td><td> x </td><td> ".$a." </td><td> = </td><td>" .$r." </td></tr>";
+                    } echo "</table>";
+                ?>
+            
+
+
+
+                <!-- checkbox -->
+                <form method="post">
+                    <h3>Revision</h3>
+                    <?php
+                        for ($i=1;$i<=10;$i++){
+                            
+                            echo("<INPUT TYPE='checkbox' NAME='num[]' VALUE='$i'/>");
+                            echo(" <label class='checkmark' for='$i'>$i</label> ");
+                        }
+                    ?>
+                    <input type="submit" value="Affichez" />
+
+                </form>
+                
+                <?php
+
+                    if (isset($_POST["num"])) {
+                        $a=$_POST["num"];
+                        // var_dump($a);
+                        $length = count($a);
+                        
+                        for($i=0; $i<$length; $i++){
+                            // afficher chaque table 
+
+                            echo "<h4>Table de multiplication de ".$a[$i]." <h4>";
+                            echo "<table>";
+
+                            for($j=1;$j<=10;$j++) {
+                                $r=$a[$i]*$j;
+                                echo "<tr><td> ".$j." </td><td> x </td><td> ".$a[$i]." </td><td> = </td><td>" .$r. "</td></tr>";
+                            }echo "</table>";    
+
+                        } 
+                        
+                    } 
+                    
+                ?>
+            </div>
+
+            <header>
+                
+                <ul id="navigation">
+                    <li> <a href="#"> Top-page </a></li>
+                    <li><a  href="quiz.php">Quiz</a><li>
+                </ul>
+            <header>
+        
+        </section>
+    </body>
+</html>
---
+++ quiz.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Quiz</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+
+         <link href="https://fonts.googleapis.com/css?family=Gugi" rel="stylesheet">  
+         <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+         <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> 
+         <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet"> 
+        
+    </head>
+    <body>
+        
+        <section class="container text-center">
+
+            <header class="text-center">
+                <ul id="navigation">
+                    <li> <a  href="index.php">Accueil</a></li>
+                    <li><a  disabled="disabled">Quiz</a><li>
+                </ul>
+
+                <h3>Quiz tables de multiplication</h3>
+
+            </header>
+            
+
+            <div class=" text-center">
+
+                <div id='myQuiz' class="col-12 "><form method="post">
+                    
+                    
+                    <?php
+                        echo "<span>Veuillez choisir votre tables de multiplication: </span><SELECT id='selectQuiz' name='select' value='.$i.'>";
+                        for($i=1; $i<=10; $i++){    
+                            echo "<OPTION>$i</OPTION>";
+                        }
+                        echo "</SELECT>";            
+                    ?>
+                    
+                    <input type ="submit" name="valid" value="Valider">
+                </form></div>
+
+
+                <div id="question"  >
+                
+                    <form   method="post">
+
+                        <?php
+                            
+                            if(isset($_POST['valid'])){  
+                                $nbrRand = 5;
+
+                                
+                                for($valuRandom=1; $valuRandom<= $nbrRand; $valuRandom++){
+                                    $random = rand(1,10);
+                                    echo '<lu>
+
+                                        <li><span class="col-6">'.$random.' x '.$_POST['select']. '</span> </li>
+                                        <li> <input class="col-6" type="text" name="result[]"></li>
+                                        <li><input type="hidden" name="randomHide[]" value="'.$random.'"></li>
+                                    </lu>';
+                                    
+                                } 
+                        
+                            }
+
+                        ?>
+     
+                        <input type="hidden" name="valueHide" value="<?php echo $_POST['select']; ?>">
+                        <input type ="submit" name="validResult" value="Resultat">
+                    </form>
+                </div>
+                <?php
+                    if(isset($_POST['validResult'])){
+
+                        
+                        
+                        $randomHide = $_POST['randomHide'];
+                        $valueHide  = $_POST['valueHide'];
+                        $results    = $_POST['result'];
+                        $score      = 0;
+
+                        for($i=0; $i< 5; $i++){
+
+                            if(($randomHide[$i] * $valueHide) == $results[$i]) {
+
+                                $score++;
+
+                            }
+
+
+                        }
+                        
+                        
+                        if($score == 5){
+                            echo('<h3 id="score" >Bien joué 5 / 5</h3>');
+                        }
+                        else{
+                            echo '<h3  id="score" >Score =  '.$score.' / 5</h5>';
+                        }
+                        
+                    }
+                ?> 
+
+            </div>
+        
+        </section>
+
+        <section>
+
+        <section>
+        
+    </body>
+</html>
